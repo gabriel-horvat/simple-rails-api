1b9c27f64db612025e08fa1b6b3f7763e0ad121e
Gabmeister <gabriel@gabrielhorvat.com>
2019-12-14 19:36:20 +0100

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+joe = User.create( username: 'joe', password: 'password' )
+fact_one = Fact.create( fact: 'Why is a raven like a writing desk?', likes: 1, user_id: 1 )
+fact_two = Fact.create( fact: 'Bla bli blup', likes: 2, user_id: 1 )
